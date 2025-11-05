d2172bd72a33f475c48447f993f39f3fa8327898
hyeju-hong <hyeju1440@naver.com>
2025-11-05 22:00:55 +0900

    remove submodule reference

---
+++ artauction/js/main.js
@@ -0,0 +1,27 @@
+$(function(){
+    AOS.init();
+});
+
+//슬라이드 효과 걸거임.  .main_visual ul.slide
+// let slideI =0; //우선 변수 만든거
+// const leng = $('.main_visual ul.slide').length -1;
+// 위에는 넘겨야 할 상황이 많을 때 근데 못생겨서 잘 안씀
+
+
+// setInterval(function(){
+//     if(slideI<3){
+//         slideI++;
+//     }else{
+//         slideI =0;
+//     }
+//     // console.log(slideI);
+
+//     $('.main_visual ul.slide').animate({
+//         left : (slideI * -100) + '%',
+//         // left : `${slideI*(-100)}%`, 이거랑 같음 
+//     },500);
+
+// }, 2900);
+
+
+
